60253e56ad2816c0265caee24c4b5cc81debcce8
LionLy <lylion98@gmail.com>
2023-03-25 23:43:54 +0100

    car counter commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # object_detection_101
 
-This project serves to introduce the basics of object detection
+This project serves to introduce the basics of object detection and applies them to perform a car counter with yolov8.
 
 ## Version
 Version 0.1.0 - 24.03.2023
@@ -8,7 +8,8 @@ Version 0.1.0 - 24.03.2023
 ## Contributor
 Lion Ly
 
-## Requirements
-poetry install
-
-Test
+## Requirements and dependencies
+First install all dependencies with `poetry install`.
+After that you need to `poe force-cudall` which is a task runner (Poe the Poet) that works well with poetry. This allows you to run any arbitrary command such as pip installations.
+We do that in order to use PyTorch wheels repo to utilize CUDA GPU. 
+(Reference: https://github.com/nat-n/poethepoet)
--- car_counter/main.py
+++ car_counter/main.py
@@ -1,35 +1,44 @@
 from ultralytics import YOLO
 import cv2
 import cvzone
+import torch
 
 def convert_to_int(*inputs):
     result = tuple(int(x) for x in inputs)
     return result
 
-cap = cv2.VideoCapture(0) # defines the video object 
-cap.set(3, 1920) # width
-cap.set(4, 1080) # height
+cap = cv2.VideoCapture("data/cars.mp4")
 
 # Initializes model with specified weights
 model = YOLO("./yolo_weights/yolov8l.pt")
 
+# Check if GPU can be found
+if torch.cuda.is_available():
+    print(torch.cuda.get_device_name())
+else:
+    print("GPU not found. Running on CPU.")
 
-class_names = ["Lioozy", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+
+class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "iPhone 13 Pro Sierra Blau 256GB",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "Phone",
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
               "teddy bear", "hair drier", "toothbrush"
               ]
 
-# Turn on Webcam
+desired_cls = ["car", "motorbike", "bus", "truck"]
+
+mask = cv2.imread("./data/mask.png")
+
 while True:
     success, img = cap.read()
-    results = model(img, stream=True) # stream=True for using generators, more efficient
+    img_region = cv2.bitwise_and(img, mask) # define a certain region to detect on only
+    results = model(img_region, stream=True) # stream=True for using generators, more efficient
 
     for r in results:
         # boundingboxes
@@ -39,14 +48,8 @@ while True:
             # Coordinates for bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = convert_to_int(x1, y1, x2, y2)
-            w, h = x2-x1, y2-y1 
-
-            # Create boxes with opencv
-            # cv2.rectangle(img, (x1,y1), (x2,y2), color=(255,0,255), thickness=3)
-
-            # Create boxes with cvzone
-            cvzone.cornerRect(img, (x1,y1,w,h), colorR=(255,0,255), colorC=(100,50,250), rt=1, t=2)
-
+            w, h = x2-x1, y2-y1     
+            
             # Confidence
             conf = round(float(box.conf[0]), 2)
 
@@ -54,9 +57,11 @@ while True:
             clf_idx = box.cls[0]
             clf_text = class_names[int(clf_idx)]
 
-            # Add text to bounding box
-            cvzone.putTextRect(img, f"{clf_text}:{conf}", (max(0,x1), max(42,y1)), scale=1.2, thickness=2)
+            if clf_text in desired_cls and conf > 0.3:
+                # Create and add text to bounding box
+                cvzone.cornerRect(img, (x1,y1,w,h), colorR=(255,0,255), colorC=(100,50,250), rt=1, t=2, l=7)
+                cvzone.putTextRect(img, f"{clf_text}:{conf}", (max(0,x1), max(42,y1)), scale=0.8, thickness=1, offset=2)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1) # 1=live video
+    cv2.waitKey(0) # 1=live video 0=press to move
 
